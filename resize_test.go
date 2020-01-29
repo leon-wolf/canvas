af66474d16d06f7bc02aeacfef9dfbd3f4d36af5
leon3103 <leon.wolf@outlook.com>
2020-01-29 09:41:55 +0100

    Checks for all Interpolation functions

--- resize_test.go
+++ resize_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 var img = image.NewNRGBA(image.Rect(0, 0, 5, 5))
+var kernels = []InterpolationFunction{CatmullRom, Bilinear}
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -16,29 +17,37 @@ func init() {
 }
 
 func Test_SameSize(t *testing.T) {
-	newImg, _ := Resize(img, 0, 0, CatmullRom)
-	if newImg.Bounds() != img.Bounds() {
-		log.Print(img.Bounds().Dx(), "x", img.Bounds().Dy())
-		log.Print(newImg.Bounds().Dx(), "x", newImg.Bounds().Dy())
-		t.Fail()
+	for _, kernel := range kernels {
+		newImg, _ := Resize(img, 0, 0, kernel)
+		if newImg.Bounds() != img.Bounds() {
+			log.Print(img.Bounds().Dx(), "x", img.Bounds().Dy())
+			log.Print(newImg.Bounds().Dx(), "x", newImg.Bounds().Dy())
+			t.Fail()
+		}
 	}
 }
 
 func Test_Height(t *testing.T) {
-	newImg, err := Resize(img, 0, 100, CatmullRom)
-	if err != nil || newImg.Bounds().Dy() != 100 || newImg.Bounds().Dx() != 100 {
-		t.Fail()
+	for _, kernel := range kernels {
+		newImg, err := Resize(img, 0, 100, kernel)
+		if err != nil || newImg.Bounds().Dy() != 100 || newImg.Bounds().Dx() != 100 {
+			t.Fail()
+		}
 	}
 }
 func Test_Width(t *testing.T) {
-	newImg, err := Resize(img, 200, 0, CatmullRom)
-	if err != nil || newImg.Bounds().Dy() != 200 || newImg.Bounds().Dx() != 200 {
-		t.Fail()
+	for _, kernel := range kernels {
+		newImg, err := Resize(img, 200, 0, kernel)
+		if err != nil || newImg.Bounds().Dy() != 200 || newImg.Bounds().Dx() != 200 {
+			t.Fail()
+		}
 	}
 }
 func Test_WidthAndHeight(t *testing.T) {
-	newImg, err := Resize(img, 300, 300, CatmullRom)
-	if err != nil || newImg.Bounds() != image.Rect(0, 0, 300, 300) {
-		t.Fail()
+	for _, kernel := range kernels {
+		newImg, err := Resize(img, 300, 300, kernel)
+		if err != nil || newImg.Bounds() != image.Rect(0, 0, 300, 300) {
+			t.Fail()
+		}
 	}
 }
